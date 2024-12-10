3f31228095c115ac01ec673313df910291e84d6f
trueSlav <2001idercach@gmail.com>
2024-12-10 17:27:55 +0300

    feat(emailEditor): add get selection text function
    the function writes the selected text to the variable

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,18 +1,52 @@
-import styles from 'EmailEditor.module.scss'
+import styles from './EmailEditor.module.scss'
 import { Bold, Italic, Strikethrough, Underline } from 'lucide-react'
+import { useRef, useState } from 'react'
 
 function EmailEditor() {
+	const [text, setText] = useState(
+		`Lorem ipsum dolor sit amet, consectetur adipisicing elit. `
+	)
+
+	const textRef = useRef<HTMLTextAreaElement | null>(null)
+
+	const getSelectionText = () => {
+		if (!textRef.current) return
+
+		const cursorStart = textRef.current?.selectionStart
+		const cursorEnd = textRef.current?.selectionEnd
+
+		const selectedText = text.substring(cursorStart, cursorEnd)
+
+		console.log(selectedText)
+	}
+
 	return (
 		<div className={styles.editor}>
-			<h1 className={styles.title}>
-				<p> Email Editor </p>
-			</h1>
+			<textarea
+				value={text}
+				ref={textRef}
+				className={styles.textZone}
+				onClick={getSelectionText}
+			>
+				{text}
+			</textarea>
 			<div className={styles.iconblock}>
-				<Bold />
-				<Italic />
-				<Underline />
-				<Strikethrough />
+				<button className='button'>
+					<Bold />
+				</button>
+				<button className='button'>
+					<Italic />
+				</button>
+				<button className='button'>
+					<Underline />
+				</button>
+				<button className='button'>
+					<Strikethrough />
+				</button>
 			</div>
+			<button className='button'>
+				<p>Send now</p>
+			</button>
 		</div>
 	)
 }
